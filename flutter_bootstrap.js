4871c39d275d6d9734247bc28f3d369122eb2aa1
tonewoodSwitzerland <admin@tischkicker.app>
2025-09-07 14:07:51 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d7f80a59fe9bf50f962180e927fa193dc3ac61
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "942818321"
+    serviceWorkerVersion: "1637279647"
   }
 });
