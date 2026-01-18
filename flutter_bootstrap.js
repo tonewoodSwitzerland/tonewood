6af4fbb90f8f67bee56c4434a670628a45440563
saegewerkschaible <saegewerk.schaible@gmail.com>
2026-01-18 20:17:06 +0100

    Built web
    Branch: master
    Commit: b30929bb10925f5f22035c31b4a75a2ad05d11f0
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a2bd0045b5a77d471254e1a54379510a08380b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1981840435"
+    serviceWorkerVersion: "1511818737"
   }
 });
